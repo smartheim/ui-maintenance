9c27c9bf028277f317cc5cfde2a37933781087cf
David Graeff <david.graeff@web.de>
2019-02-24 14:56:53 +0100

    importModule: Fix for: multiple sources requested the same js module

--- js/_common/importModule.js
+++ js/_common/importModule.js
@@ -3,39 +3,34 @@
  * It will with Firefox 67 (May 2019). Please remove this polyfill after
  * the release.
  */
-export function importModule(url) {
-  return new Promise((resolve, reject) => {
-    const vector = "import" + url.replace(/\./g, "").replace(/\//g, "_");
-    if (document.getElementById("id_" + vector)) {
-      if (window[vector]) {
-        resolve(window[vector]);
-      } else {
-        reject(new Error(`Import failed earlier: ${url}`));
-      }
-      return;
+export async function importModule(url) {
+  const vector = "import" + url.replace(/\./g, "").replace(/\//g, "_");
+  if (document.getElementById("id_" + vector)) {
+    return window[vector];
+  }
+
+  const loader = `
+  import * as m from "${url}";
+  window["${vector}"] = m;
+  document.getElementById("id_${vector}").dispatchEvent(new CustomEvent("loaded",{detail:m}))
+  `; // export Module
+
+  const script = document.createElement("script");
+  script.type = "module";
+  script.id = "id_" + vector;
+  script.async = 'async';
+  script.textContent = loader;
+
+  const promise = new Promise((resolve, reject) => {
+    script.onerror = (e) => {
+      console.warn(`Failed to import: ${url}`, e);
+      reject(new Error(`Failed to import: ${url}`));
     }
-
-    const loader = `
-    import * as m from "${url}";
-    window["${vector}"] = m;
-    document.getElementById("id_${vector}").dispatchEvent(new CustomEvent("loaded",{detail:m}))
-    `; // export Module
-
-    const script = document.createElement("script");
-    script.type = "module";
-    script.id = "id_" + vector;
-    script.async = 'async';
-    script.textContent = loader;
-
-    script.onerror = () => reject(new Error(`Failed to import: ${url}`));
     script.addEventListener("loaded", (event) => {
       resolve(event.detail);
     }, { passive: true });
-    try {
-      document.head.appendChild(script);
-    } catch (e) {
-      console.warn("failed", e);
-      reject(new Error(`Failed to import: ${url}`));
-    }
+    document.head.appendChild(script);
   });
+  window[vector] = promise;
+  return promise;
 }
